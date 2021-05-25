3aaac791963895f8d4b3b39a71c64e92bb8bb078
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-05-25 11:00:18 +0100

    layout, header and footer

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout'
+
+export default function AboutPage() {
+	return (
+		<Layout title='About DJ events'>
+			<h1>About Page</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti, sed rem? Quos temporibus rem autem tenetur illo, ex culpa, vel id ullam non optio repellendus dolorem libero minus voluptatem voluptatum!</p>
+            <p>V0.1</p>
+		</Layout>
+	);
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout'
+
+export default function AddEventsPage() {
+    return (
+        <Layout title='Add a new event'>
+            <h1>Add Events</h1>
+        </Layout>
+    )
+}
