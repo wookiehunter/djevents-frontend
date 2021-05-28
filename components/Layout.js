bb450329c006c9cd3ddea5f1f7f6558ceb98883e
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-05-28 14:39:09 +0100

    wookie

--- components/Layout.js
+++ components/Layout.js
@@ -25,7 +25,7 @@ export default function Layout({title, keywords, description, children}) {
 }
 
 Layout.defaultProps = {
-    title: 'DJ Events | Find the hottest parties',
+    title: 'Wookie Events | Find the hottest parties',
     description: 'Find the latest DJ  and other musical events',
     keywords: 'music, dj, edm. events'
 }
