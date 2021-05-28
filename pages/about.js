1bed1552077408ffa15103dd9368bb5547c7d3a6
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-05-28 13:27:15 +0100

    wookie

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ export default function AboutPage() {
 	return (
 		<Layout title='About DJ events'>
 			<h1>About Page</h1>
-            <p>This demo project has been built and deployed with U+1F49C by Steve @ <a href="https://xenos-design.co.uk">Xenos-Design &trade;</a></p>
+            <p>This demo project has been built and deployed by Steve @ <a href="https://xenos-design.co.uk">Xenos-Design &trade;</a></p>
             <p>V0.1</p>
 		</Layout>
 	);
